926dd521ed5987eb1892425475c5750b584d0fce
Hazel Anne Villareal <hae.villareal@gmail.com>
2020-03-16 20:21:04 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution here!
+
+const pets = ["Milo", "Otis", "Garfield"]
+const append = pets.slice(), prepend = pets.slice(), removeLast = pets.slice(), removeFirst = pets.slice()
+
+append.push("Odie");
+
+prepend.unshift("Odie");
+
+removeLast.pop();
+
+removeFirst.shift();
